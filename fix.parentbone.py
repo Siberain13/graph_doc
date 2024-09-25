2c7ba3206c273f1e5f114b3952fef0541bbec246
Siberain13 <107906950+Siberain13@users.noreply.github.com>
2024-09-25 16:58:50 +0700

    Update fix.parentbone.py

--- fix.parentbone.py
+++ fix.parentbone.py
@@ -1,25 +1,47 @@
 import bpy
+import csv
+#import pathlib
 
-obj = bpy.data.objects['Cube']
-armature = bpy.data.objects['Armature']
-parent_bone = 'Bone.001'  # choose the bone name which you want to be the parent
+file_Path = 'C:\\Users\\d.suniwat\\Desktop\\link.csv'
 
-bpy.ops.object.select_all(action='DESELECT')
+bpy.ops.object.mode_set(mode='EDIT', toggle=False)
 
-armature.select_set(True)
-bpy.context.view_layer.objects.active = armature
+name_list =[]
 
-bpy.ops.object.mode_set(mode='EDIT')
-armature.data.edit_bones.active = armature.data.edit_bones[parent_bone]
+with open(file_Path,mode='r',newline='') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        name_list.append(str(row[0]))
 
-bpy.ops.object.mode_set(mode='OBJECT')
+#print(name_list)
 
-bpy.ops.object.select_all(action='DESELECT')  # deselect all objects
-obj.select_set(True)
-armature.select_set(True)
-bpy.context.view_layer.objects.active = armature
-# the active object will be the parent of all selected object
+for name in name_list:
+    
+    bpy.ops.object.mode_set(mode='OBJECT')  
+      
+    obj = bpy.data.objects[name]
+    armature = bpy.data.objects['Armature']
+    parent_bone = name  # choose the bone name which you want to be the parent
 
-bpy.ops.object.parent_set(type='BONE_RELATIVE', keep_transform=True)
+    bpy.ops.object.select_all(action='DESELECT')
+
+    armature.select_set(True)
+    bpy.context.view_layer.objects.active = armature
+    
+    print(name)
+
+    bpy.ops.object.mode_set(mode='EDIT')
+    armature.data.edit_bones.active = armature.data.edit_bones[parent_bone]
+
+    bpy.ops.object.mode_set(mode='OBJECT')
+
+    bpy.ops.object.select_all(action='DESELECT')  # deselect all objects
+    obj.select_set(True)
+    armature.select_set(True)
+    bpy.context.view_layer.objects.active = armature
+    # the active object will be the parent of all selected object
+
+    bpy.ops.object.parent_set(type='BONE_RELATIVE', keep_transform=True)
+
 
 SOURCE::https://blender.stackexchange.com/questions/77465/python-how-to-parent-an-object-to-a-bone-without-transformation?newreg=58384217f5ed47e18e6987bbe79e7cb6
