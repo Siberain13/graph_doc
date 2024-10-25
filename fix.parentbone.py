19d987f2f18ff27bf9f116c65d236ad14485f94b
Siberain13 <107906950+Siberain13@users.noreply.github.com>
2024-10-25 16:41:25 +0700

    Update fix.parentbone.py

--- fix.parentbone.py
+++ fix.parentbone.py
@@ -7,28 +7,34 @@ file_Path = 'C:\\Users\\d.suniwat\\Desktop\\link.csv'
 bpy.ops.object.mode_set(mode='EDIT', toggle=False)
 
 name_list =[]
+bone_list =[]
 
 with open(file_Path,mode='r',newline='') as f:
     reader = csv.reader(f)
     for row in reader:
-        name_list.append(str(row[0]))
+        bone_list.append(str(row[0]))
+        name_list.append(str(row[1]))
 
 #print(name_list)
 
-for name in name_list:
+#for name in name_list:
+i = 0
+while i < len(name_list):
+    namen = name_list[i]
+    bonen = bone_list[i]
     
     bpy.ops.object.mode_set(mode='OBJECT')  
       
-    obj = bpy.data.objects[name]
+    obj = bpy.data.objects[namen]
     armature = bpy.data.objects['Armature']
-    parent_bone = name  # choose the bone name which you want to be the parent
+    parent_bone = bonen  # choose the bone name which you want to be the parent
 
     bpy.ops.object.select_all(action='DESELECT')
 
     armature.select_set(True)
     bpy.context.view_layer.objects.active = armature
     
-    print(name)
+    print(namen)
 
     bpy.ops.object.mode_set(mode='EDIT')
     armature.data.edit_bones.active = armature.data.edit_bones[parent_bone]
@@ -43,5 +49,7 @@ for name in name_list:
 
     bpy.ops.object.parent_set(type='BONE_RELATIVE', keep_transform=True)
 
+    i += 1
 
-SOURCE::https://blender.stackexchange.com/questions/77465/python-how-to-parent-an-object-to-a-bone-without-transformation?newreg=58384217f5ed47e18e6987bbe79e7cb6
+
+#SOURCE::https://blender.stackexchange.com/questions/77465/python-how-to-parent-an-object-to-a-bone-without-transformation?newreg=58384217f5ed47e18e6987bbe79e7cb6
